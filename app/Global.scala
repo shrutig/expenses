6adeb45459cd5bb805c1c522ed417456f2b13948
shrutig <shruti.999.999@gmail.com>
2015-11-04 10:52:04 +0530

    Global.scala changed

--- app/Global.scala
+++ app/Global.scala
@@ -1,16 +1,8 @@
-import org.mindrot.jbcrypt.BCrypt
-import play.api.Application
-import play.api.db.DB
-import org.mindrot.jbcrypt.BCrypt
+
 import play.api.Play.current
 import play.api.Application
 import play.api.GlobalSettings
 import play.api.db.DB
-import play.api.mvc.{Handler, RequestHeader}
-import play.api.mvc.Results._
-import scala.collection.mutable.ListBuffer
-import scala.concurrent.Future
-import play.api.i18n.{ Messages, Lang }
 
 
 
@@ -20,13 +12,23 @@ object Global extends GlobalSettings {
     val START_STATE_1= "select count(*) from employee where role = 'super';"
     val START_STATE_2= "INSERT INTO employee VALUES('','super');"
     val START_STATE_3= "update employee set role='super' where userName='';"
+    val START_STATE_4 ="select count(*) from employee where userName='';"
     DB.withConnection { conn =>
       val stmt = conn.prepareStatement(START_STATE_1)
       val rs = stmt.executeQuery()
+      val stmt1 = conn.prepareStatement(START_STATE_4)
+      val rs1 = stmt1.executeQuery()
       rs.next()
+      rs1.next()
       if (rs.getInt(1) == 0) {
-        val stmt1 = conn.prepareStatement(START_STATE_2)
-        stmt1.executeUpdate()
+        if(rs1.getInt(1) == 0) {
+          val stmt2 = conn.prepareStatement(START_STATE_2)
+          stmt2.executeUpdate()
+        }
+        else{
+          val stmt2 = conn.prepareStatement(START_STATE_3)
+          stmt2.executeUpdate()
+        }
       }
     }
     super.onStart(app)
